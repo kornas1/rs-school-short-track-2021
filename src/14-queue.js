a049b2874de7be4c3aafcb57a9b61cc1f84360f6
kornas1 <79769071+kornas1@users.noreply.github.com>
2021-04-12 23:19:50 +0300

    feat: add 14

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,28 +1,34 @@
 // const ListNode = require('../extensions/list-node');
 /**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- *
- */
+* Implement the Queue with a given interface via linked list (use ListNode extension above).
+*
+* @example
+* const queue = new Queue();
+*
+* queue.enqueue(1); // adds the element to the queue
+* queue.enqueue(3); // adds the element to the queue
+* queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+*
+*/
+
+const ListNode = require('../extensions/list-node');
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
-  }
+constructor() {
+this.queque = [];
+}
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
-  }
+get size() {
+return this.queque.length;
+}
 
-  dequeue() {
-    throw new Error('Not implemented');
-  }
+enqueue(element) {
+this.queque.push(new ListNode(element));
+}
+
+dequeue() {
+return this.queque.shift().value;
+}
 }
 
 module.exports = Queue;
